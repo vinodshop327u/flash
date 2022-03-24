664d23c040b67a4fb2cdc1dc387582412f52d5bb
vinod <vinodshop327u@gmail.com>
2022-03-24 15:58:07 +0530

    first commit uploading all files

---
+++ CoreJava1/src/com/vinod/io/Sample.java
@@ -0,0 +1,9 @@
+package com.vinod.io;
+
+public class Sample 
+{
+	public static void main(String[] args) {
+		System.out.println("hai ");
+	}
+
+}
---
+++ CoreJava1/src/com/vinod/io/Sample2.java
@@ -0,0 +1,5 @@
+package com.vinod.io;
+
+public class Sample2 {
+
+}
