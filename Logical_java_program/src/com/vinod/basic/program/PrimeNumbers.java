8b63a6f79e2413f18573d6688cc6dc768c5d7c1c
vinod <vinodshop327u@gmail.com>
2022-05-02 18:35:25 +0530

    add Prime file

---
+++ Logical_java_program/src/com/vinod/basic/program/PrimeNumbers.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+package com.vinod.basic.program;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) 
+	{
+		int i,m=0,flag=0;      
+		  int n=149;//it is the number to be checked    
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   System.out.println(n+" is not prime number If");      
+		  }else{  
+		   for(i=2;i<=m;i++){ 
+			  // System.out.println(" i = "+i);
+		    if(n%i==0){      
+		     System.out.println(n+" is not prime number");      
+		     flag=1;      
+		     break;      
+		    }      
+		   }      
+		   if(flag==0)  { System.out.println(n+" is prime number"); }  
+		  }//end of else  
+	}
+
+}
