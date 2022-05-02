10dfb7e0a46b185c850636973e4e14539bdcd784
vinod <vinodshop327u@gmail.com>
2022-05-02 19:38:18 +0530

    add files

---
+++ Logical_java_program/src/com/vinod/basic/program/AlphabetPattern.java
@@ -0,0 +1,28 @@
+package com.vinod.basic.program;
+
+public class AlphabetPattern 
+{
+	public static void main(String[] args)  
+	{  
+		int i, j, n=10;  
+		// Outer for loop for number of lines  
+		for (i = 0; i<=n; i++)   
+		{  
+			// Inner for loop for logic execution  
+			for (j = 0; j<= n / 2; j++)   
+			{  
+				// prints two vertical lines  
+				if ((j == 0 || j == n / 2) && i != 0 ||  
+						// print first line of alphabet  
+						i == 0  && j != n / 2 ||   
+						// prints middle line  
+						i == n / 2)   
+					System.out.print("*");  
+				else  
+					System.out.print(" ");  
+			}  
+			System.out.println();  
+		}  
+	}  
+
+}
---
+++ Logical_java_program/src/com/vinod/basic/program/Number_Pattern1.java
@@ -0,0 +1,25 @@
+package com.vinod.basic.program;
+
+public class Number_Pattern1 
+{
+	public static void main(String args[])   
+	{   
+		int i, j,number, n=10;   
+		//loop for rows  
+		for(i=0; i<n; i++)  
+		{   
+			number=1;   
+			//loop for columns  
+			for(j=0; j<=i; j++)  
+			{   
+				//prints num  
+				System.out.print(number+ " ");   
+				//incrementing the value of number   
+				number++;   
+			}   
+			//throws the cursor at the next line after printing each row  
+			System.out.println();   
+		}   
+	}   
+
+}
