3b67cd2db236e43d8e405a7cf3842d1a5b37014b
vinod <vinodshop327u@gmail.com>
2022-05-02 18:38:49 +0530

    add prime numbers

---
+++ Logical_java_program/src/com/vinod/basic/program/PrimeNumber_method.java
@@ -0,0 +1,31 @@
+package com.vinod.basic.program;
+
+public class PrimeNumber_method 
+{
+	static int i,m=0,flag=0;
+	static void primenumber(int n)
+	{
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   System.out.println(n+" is not prime number If");      
+		  }else{  
+		   for(i=2;i<=m;i++){ 
+			  // System.out.println(" i = "+i);
+		    if(n%i==0){      
+		     System.out.println(n+" is not prime number");      
+		     flag=1;      
+		     break;      
+		    }      
+		   }      
+		   if(flag==0)  { System.out.println(n+" is prime number"); }  
+		  }//end of else  
+	}
+	public static void main(String[] args) 
+	{
+		primenumber(2);
+		primenumber(5);
+		primenumber(10);
+		
+	}
+
+}
