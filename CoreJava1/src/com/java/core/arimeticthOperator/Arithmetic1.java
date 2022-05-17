c8a1be3a1814832b34abd3cce03c1740a93c7782
vinod <vinodshop327u@gmail.com>
2022-05-17 10:25:30 +0530

    update

--- CoreJava1/src/com/java/core/arimeticthOperator/Arithmetic1.java
+++ CoreJava1/src/com/java/core/arimeticthOperator/Arithmetic1.java
@@ -16,7 +16,7 @@ public class Arithmetic1
 		System.out.println(10<<3);//10*2^3=10*8=80  
 		System.out.println(20<<2);//20*2^2=20*4=80  
 		System.out.println(15<<4);//15*2^4=15*16=240  
-		
+		System.out.println(15<<4);
 	}
 
 }
