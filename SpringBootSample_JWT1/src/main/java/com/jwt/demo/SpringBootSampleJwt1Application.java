e3ebb96e4312fc787cb734015afbd771a92c4d33
jasvin <jasvin@jasvin-PC>
2022-07-20 04:26:40 +0530

    Jwt sample Example

---
+++ SpringBootSample_JWT1/src/main/java/com/jwt/demo/SpringBootSampleJwt1Application.java
@@ -0,0 +1,13 @@
+package com.jwt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleJwt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleJwt1Application.class, args);
+	}
+
+}
